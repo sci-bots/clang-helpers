eea7824076b83d6a9607f5bfbf91f30ee0ef5aa2
Christian Fobel <christian@fobel.net>
2014-05-01 13:32:59 -0400

    Initial commit of `clang_helpers` package.

---
+++ clang_helpers/__init__.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+
+from ctypes.util import find_library
+from clang.cindex import CursorKind
+import clang.cindex
+
+clang.cindex.Config.set_library_file(find_library('clang'))
+
+
+def extract_method_signature(method_cursor):
+    definition = method_cursor.get_definition()
+    return (method_cursor.displayname,
+            definition.result_type.get_canonical().kind,
+            [(a.type.get_canonical().kind, a.displayname)
+             for a in definition.get_arguments()])
+
+
+def extract_method_signatures(class_cursor):
+    method_signatures = [extract_method_signature(m)
+                         for m in class_cursor.get_children()
+                         if m.kind == CursorKind.CXX_METHOD]
+
+    methods = OrderedDict()
+
+    for s in method_signatures:
+        method_name = s[0][:s[0].index('(')]
+        signatures = methods.setdefault(method_name, [])
+        signatures.append(s[1:])
+    return methods
+
+
+def extract_class_declarations(root_cursor):
+    return OrderedDict([(n.displayname, n)
+                        for n in root_cursor.get_children()
+                        if n.kind == CursorKind.CLASS_DECL])
+
+
+def open_cpp_source(source_path, *args, **kwargs):
+    index = clang.cindex.Index.create()
+    translational_unit = index.parse('Node.h', ['-x', 'c++'] + list(args),
+                                     **kwargs)
+
+    return translational_unit.cursor
---
+++ pavement.py
@@ -0,0 +1,22 @@
+from paver.easy import task, needs
+from paver.setuputils import setup, install_distutils_tasks
+
+import version
+
+
+setup(name='clang_helpers',
+      version=version.getVersion(),
+      description='High-level API using `clang` module to provide static C++ '
+      'class introspection.',
+      keywords='c++ clang introspection',
+      author='Christian Fobel',
+      url='https://github.com/wheeler-microfluidics/clang_helpers',
+      license='GPL',
+      packages=['clang_helpers', ])
+
+
+@task
+@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
