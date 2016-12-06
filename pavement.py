abae53138fc57c3f8b0a337e2cc218292b751661
Christian Fobel <christian@fobel.net>
2016-12-06 12:29:44 -0500

    Add Conda build recipe

--- pavement.py
+++ pavement.py
@@ -14,9 +14,8 @@ setup(name='clang_helpers',
       author='Christian Fobel',
       url='https://github.com/wheeler-microfluidics/clang_helpers',
       license='GPL',
-      packages=['clang_helpers', 'clang_helpers.clang'],
-      package_data={'clang_helpers': ['libclang/*']},
-      install_requires=['clang>=3.8', 'path_helpers'])
+      packages=['clang_helpers'],
+      install_requires=['path_helpers', 'pydash'])
 
 
 @task
